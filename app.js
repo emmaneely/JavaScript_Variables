5e5972dda6d1ec27363ff6467609284c085c8020
emmaneely <emmaneely3@gmail.com>
2022-06-06 14:59:43 -0500

    Updated exercise 1 and added template strings

--- app.js
+++ app.js
@@ -4,27 +4,36 @@ console.log(
 );
 
 // Exercise 1
-// const firstName = "";
-// let lastName = "";
-// var age = ;
+// const firstName = "Emma";
+let lastName;
+var age;
 // Exercise 2
 const firstName = "Emma";
-let lastName = "Neely";
-var age = 33;
+lastName = "Neely";
+age = 33;
 // Exercise 3
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
 // Exercise 4
+let product = "T-shirt";
 let price = 19.99;
 let isOnSale = false;
 let salePercentage = 15;
 let stock = 0;
 let inStock = false;
 let selectedSize = "M";
+
+console.log(`${product} in size ${selectedSize} is ${price} with ${stock} in stock.`);
+
 // Exercise 5
 let title = "Name of the Wind";
 let author = "Patrick Rothfuss";
 let pageCount = 722;
 let bookmark = 456;
 let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long.`);
